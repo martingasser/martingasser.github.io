2f17d703de549de592241d06afdb153f6689a422
Martin Gasser <martin@m1xbook.local>
2023-11-22 18:06:26 +0100

    update

--- .output/public/_nuxt/DocumentDrivenNotFound.ba8f2284.js
+++
@@ -1,1 +0,0 @@
-import{f as n,l as e}from"./entry.84228106.js";const t=n({name:"DocumentDrivenNotFound",render(){return e("div","Document not found")}});export{t as default};
---
+++ .output/public/works/bachmann-kuppel/_payload.js
@@ -0,0 +1,1 @@
+export default {data:{},prerenderedAt:1700672782250}
