12c8830cfd39dfa8fcd105b6cc01b665c920296a
martingasser <martin.gasser@gmail.com>
2024-04-29 16:23:38 +0200

    update

---
+++ _nuxt/DocumentDrivenNotFound.34c7279e.js
@@ -0,0 +1,1 @@
+import{f as n,l as e}from"./entry.34b963c3.js";const t=n({name:"DocumentDrivenNotFound",render(){return e("div","Document not found")}});export{t as default};
---
+++ works/magicqueen/_payload.js
@@ -0,0 +1,1 @@
+export default {data:{},prerenderedAt:1714400612669}
